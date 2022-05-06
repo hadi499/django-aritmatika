7efa3f2d5f6b91360d745278b466e32405113558
hadi499 <hadi.ygcl@gmail.com>
2022-05-06 20:00:43 +0700

    update modal bootstraps

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,15 @@ def home(request):
         'posts': posts}
 
     return render(request, 'blog/index.html', context)
+
+
+def create(request):
+    post_form = PostForm(request.POST)
+
+    if request.method == 'POST':
+        if post_form.is_valid():
+            post_form.save()
+
+        return redirect('home')
+
+    return render(request, 'blog/index.html')
--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'django.contrib.humanize',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'id'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_THOUSAND_SEPARATOR = True
